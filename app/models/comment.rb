4b7365f77ab18b95cf620e0f48209749f6b88653
Michael McColllum <michaelxmccollum@gmail.com>
2019-09-25 13:20:02 +0000

    Lesson 35. Added comments with drop down rating and added code to display last comment on individual place page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,19 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :place
+
+# created a rating drop down menu starting with line 6
+  RATINGS = {
+      'one star': '1_star',
+      'two stars': '2_stars',
+      'three stars': '3_stars',
+      'four stars': '4_stars',
+      'five stars': '5_stars'
+    }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
 
 # Whenever you have an id that points to another table in your database table,
